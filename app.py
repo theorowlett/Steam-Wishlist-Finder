3e454a6001f4df6f0b2288e775d2de8b5bf0469a
theorowlett <trowlett@gmail.com>
2021-07-14 16:15:44 -0700

    app.py now references steam.py, supports returning data for results page.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import steam
 app = Flask('__name__')
 
 
@@ -6,6 +7,19 @@ app = Flask('__name__')
 def index():
     return render_template('index.html')
 
+@app.route('/result', methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        username = request.values.get('username')
+        print(username)
+        steamID = steam.get_steamid(username)
+        wishlist_games = steam.get_wishlist(steamID)
+        wishlist = steam.Wishlist(wishlist_games)
+        wishlist = steam.itad(wishlist)
+        print(wishlist)
+        return render_template('result.html',wishlist = wishlist.games)
+    else:
+        return render_template('index.html')
 app.run(debug=True)
 
 if __name__=='__main__':
